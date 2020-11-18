f91acc12d155eb7ec81c0d3c9e9089a09f8028fb
Christian Taubenheim <christian@sedna.de>
2020-11-18 16:26:12 +0100

    Modified coded to calculate a average over a given time (./monitor <time in seconds>)

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -1,62 +1,47 @@
 #include "monitor.h"
 
+constexpr auto POLLING_PERIOD{std::chrono::milliseconds(1000)};
 
-constexpr auto POLLING_PERIOD{std::chrono::milliseconds(250)};
-
-
-int main() {
+int main(int argc, char **argv)
+{
   NVML nvml;
 
   auto nvml_info = nvml.get_info();
-  std::cout << "driver version:" << "\t" << nvml_info.driver_version << "\n"
-            << "NVML version:"   << "\t" << nvml_info.nvml_version   << "\n"
-            << "\n";
-
   NVMLDeviceManager device_manager{nvml};
-
-  std::cout << "\n"
-            << "devices_count:" << "\t" << device_manager.get_devices_count() << "\n"
-            << "devices: "      << "\n";
 
   const auto devices_begin = device_manager.devices_begin();
   const auto devices_end = device_manager.devices_end();
 
-  for (auto device = devices_begin; device != devices_end; ++device) {
-    const auto& info = (*device).get_info();
-    std::cout << "- device_index:"       << "\t\t"   << info.index << "\n"
-              << "  name:"               << "\t\t\t" << info.name  << "\n"
-              << "  fan_speed:"          << "\t\t"   << info.metrics.fan_speed          << "%"  << "\n"
-              << "  temperature:"        << "\t\t"   << info.metrics.temperature        << "C"  << "\n"
-              << "  power_usage:"        << "\t\t"   << info.metrics.power_usage        << "mW" << "\n"
-              << "  gpu_utilization:"    << "\t"     << info.metrics.gpu_utilization    << "%"  << "\n"
-              << "  memory_utilization:" << "\t"     << info.metrics.memory_utilization << "%"  << "\n";
-  }
-
-  std::cout << "\n\n"
-            << "Monitoring GPUs with polling period of " << POLLING_PERIOD.count() << "ms"
-            << "\n\n\n"
-            << "timestamp_ms,index,fan_speed,temperature,power_usage,gpu_utilization,memory_utilization"
-            << "\n";
-
   std::chrono::milliseconds timestamp;
-
-  while (true) {
-    timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
-      std::chrono::high_resolution_clock::now().time_since_epoch()
-      );
-
-    for (auto device = devices_begin; device != devices_end; ++device) {
+  int size = 1;
+  if (argc == 2)
+    sscanf(argv[1], "%d", &size);
+  int cnt = 0;
+  float values[size];
+  while (true)
+  {
+    for (auto device = devices_begin; device != devices_end; ++device)
+    {
       (*device).refresh_metrics_or_halt();
-      const auto& info = (*device).get_info();
+      const auto &info = (*device).get_info();
+      if (info.index == 0)
+      {
 
-      std::cout
-        << timestamp.count()               << ","
-        << info.index                      << ","
-        << info.metrics.fan_speed          << ","
-        << info.metrics.temperature        << ","
-        << info.metrics.power_usage        << ","
-        << info.metrics.gpu_utilization    << ","
-        << info.metrics.memory_utilization << std::endl;
+        values[cnt] = info.metrics.gpu_utilization;
+        cnt += 1;
+      }
+    }
+    if (cnt >= size)
+    {
+      float avg = 0.0;
+      for (int i = 0; i < size; ++i)
+      {
+        avg += values[i];
+      }
+      avg /= size;
+      timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
+      std::cout <<timestamp.count()<<"#"<< avg << std::endl;
+      break;
     }
 
     std::this_thread::sleep_for(POLLING_PERIOD);
